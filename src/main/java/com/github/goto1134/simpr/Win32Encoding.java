139e9a2c846e6bb695c139385bd4f43c28c11b43
Andrew Yefanov <1134togo@gmail.com>
2017-09-17 01:29:37 +0300

    Make ANSI default encoding

--- src/main/java/com/github/goto1134/simpr/Win32Encoding.java
+++ src/main/java/com/github/goto1134/simpr/Win32Encoding.java
@@ -7,9 +7,9 @@ package com.github.goto1134.simpr;
 public enum Win32Encoding {
     ANSI,
     UNICODE;
-    public static final String ENCODING_IS_ANSI_FLAG = "com.github.goto1134.isANSI";
-    public static Win32Encoding systemEncoding = Boolean.getBoolean(ENCODING_IS_ANSI_FLAG) ? Win32Encoding.ANSI
-                                                                                           : Win32Encoding.UNICODE;
+    public static final String ENCODING_IS_UNICODE_KEY = "com.github.goto1134.use.unicode";
+    public static Win32Encoding systemEncoding = Boolean.getBoolean(ENCODING_IS_UNICODE_KEY) ? Win32Encoding.UNICODE
+                                                                                             : Win32Encoding.ANSI;
 
     public static boolean isANSI() {return !isUnicode();}
 
